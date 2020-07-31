43a3bb60327341fec3618b8f9f235eb3c6146bc3
Chris <withscottc921@gmail.com>
2020-07-31 08:40:56 -0600

    Initial Commit

---
+++ 500French/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  500French
+//
+//  Created by Chris Withers on 7/31/20.
+//  Copyright © 2020 Chris Withers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
